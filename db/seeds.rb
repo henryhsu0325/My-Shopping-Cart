2189b8b320e8aad009b6dd953bf4a06cadb782ff
Henry <henry81325@gmail.com>
2018-09-14 14:11:31 +0800

    setup environment for assessment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create( name: "henry", email: "asd@.asd.com", password: "123123", role: "admin")
+puts "Default admin created!”
